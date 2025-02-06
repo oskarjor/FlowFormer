2b78cdd4697603967a4d572f2c49d184019e2c21
Oskar Jorgensen <oskarjorgensen@gmail.com>
2025-02-06 17:30:04 +0100

    add minimum for running flow matching

---
+++ scripts/train_cifar.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name="train_cifar10"   # Sensible name for the job
+#SBATCH --account=ie-idi      # Account for consumed resources
+#SBATCH --partition=GPUQ
+#SBATCH --gres=gpu:1            # Number of GPUs
+#SBATCH --nodes=1               # Number of nodes
+#SBATCH --ntasks-per-node=1              # Number of tasks
+#SBATCH --time=00-00:10:00    # Upper time limit for the job (DD-HH:MM:SS)
+#SBATCH --output=train_cifar10.out
+#SBATCH --mail-user=oskarjor@ntnu.no
+#SBATCH --mail-type=ALL
+
+module load Python/3.10.15-GCCcore-14.2.0
+source /cluster/home/oskarjor/torchcfm/bin/activate
+pip install -r requirements.txt
+
+python train_cifar10.py
