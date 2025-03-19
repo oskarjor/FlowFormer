a4b34acfed356a78f5ac692fa8627d5719b03ca2
Oskar Jorgensen <oskarjorgensen@gmail.com>
2025-03-19 09:54:14 +0100

    working singleGPU version of CFM

--- scripts/cfm_train.sh
+++ scripts/cfm_train.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
-#SBATCH --job-name="train_cfm"   # Sensible name for the job
-#SBATCH --account=ie-idi      # Account for consumed resources
+#SBATCH --job-name="64x64_train_cfm"   # Sensible name for the job
+#SBATCH --account=share-ie-idi      # Account for consumed resources
 #SBATCH --partition=GPUQ
-#SBATCH --gres=gpu:1            # Number of GPUs
+#SBATCH --gres=gpu:h100:1            # Number of GPUs
 #SBATCH --nodes=1               # Number of nodes
 #SBATCH --ntasks-per-node=1              # Number of tasks
-#SBATCH --time=00-00:10:00    # Upper time limit for the job (DD-HH:MM:SS)
-#SBATCH --output=%j_train_cfm.out
+#SBATCH --time=07-00:00:00    # Upper time limit for the job (DD-HH:MM:SS)
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=100G
+#SBATCH --output=%j_train_cfm_64x64.out
 #SBATCH --mail-user=oskarjor@ntnu.no
-#SBATCH --mail-type=ALL
+#SBATCH --mail-type=NONE
 
 module load Python/3.10.8-GCCcore-12.2.0
 source /cluster/home/oskarjor/.virtualenv/flowformer/bin/activate
 
-python train_cfm/train.py
+python train_cfm/train.py \
+	--batch_size=128 \
+	--total_steps=400000 \
+	--model="otcfm" \
+	--save_dir="./results/otcfm/$SLURM_JOB_ID/" \
+	--image_size=64
