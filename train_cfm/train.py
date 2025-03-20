cf83a881272669b73c1e4a2bea4039e421614ef8
Oskar Jorgensen <oskarjorgensen@gmail.com>
2025-03-20 09:18:52 +0100

    add more debug statements

--- train_cfm/train.py
+++ train_cfm/train.py
@@ -189,7 +189,10 @@ def train(argv):
         optim.zero_grad()
         x1, y = next(datalooper)
         x1 = x1.to(device)
+        print(f"Before: {y}")
         y = y.to(device) if FLAGS.class_conditional else None
+        print(f"After: {y}")
+        print(f"FLAGS.class_conditional: {FLAGS.class_conditional}")
         x0 = torch.randn_like(x1)
         t, xt, ut = FM.sample_location_and_conditional_flow(x0, x1)
         vt = net_model(t, xt, y)
