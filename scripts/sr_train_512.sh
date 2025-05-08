ae0c959eb41455cb974091ab6d2fc5992af09181
Oskar Jorgensen <oskarjorgensen@gmail.com>
2025-05-08 10:36:00 +0200

    add new training script

---
+++ scripts/sr_train_512.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH --job-name="256->512_train_sr"   # Sensible name for the job
+#SBATCH --account=share-ie-idi      # Account for consumed resources
+#SBATCH --partition=GPUQ
+#SBATCH --gres=gpu:h100:1            # Number of GPUs
+#SBATCH --nodes=1               # Number of nodes
+#SBATCH --ntasks-per-node=1              # Number of tasks
+#SBATCH --time=00-00:10:00    # Upper time limit for the job (DD-HH:MM:SS)
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=100G
+#SBATCH --output=%j.out
+#SBATCH --mail-user=oskarjor@ntnu.no
+#SBATCH --mail-type=NONE
+
+module load Python/3.10.8-GCCcore-12.2.0
+source /cluster/home/oskarjor/.virtualenv/flowformer/bin/activate
+
+pwd
+export WANDB_API_KEY=$(cat ~/FlowFormer/.wandb_api_key)
+
+python train_SR/train.py \
+	--batch_size=16 \
+	--total_steps=400000 \
+	--model="otcfm" \
+	--save_dir="./results/otcfm/$SLURM_JOB_ID/" \
+	--pre_image_size=256 \
+	--post_image_size=512 \
+	--lr=1e-4 \
+	--wandb_name="256->512_otcfm_$SLURM_JOB_ID" \
+	--wandb_project="flowformer" \
+	--wandb_entity="oskarjor" \
+	--use_wandb=True \
+	--class_conditional=True
