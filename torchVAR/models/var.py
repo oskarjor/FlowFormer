fc408342a2914b92a38bd3b39d106a9324d2f655
Oskar Jorgensen <oskarjorgensen@gmail.com>
2025-05-08 10:31:20 +0200

    update var.py

--- torchVAR/models/var.py
+++ torchVAR/models/var.py
@@ -265,7 +265,6 @@ class VAR(nn.Module):
             f_hat, next_token_map = self.vae_quant_proxy[
                 0
             ].get_next_autoregressive_input(si, len(self.patch_nums), f_hat, h_BChw)
-            print(f"f_hat.shape: {f_hat.shape}")
             if si != self.num_stages_minus_1:  # prepare for next stage
                 next_token_map = next_token_map.view(B, self.Cvae, -1).transpose(1, 2)
                 next_token_map = (
@@ -279,14 +278,15 @@ class VAR(nn.Module):
             ms_idx_Bl.append(idx_Bl)
             ms_h_BChw.append(h_BChw)
 
-        # results = self.vae_proxy[0].idxBl_to_img(
-        #     ms_idx_Bl, same_shape=False, last_one=False
-        # )
-        # results = [
-        #     result.add_(1).mul_(0.5) for result in results
-        # ]  # de-normalize, from [-1, 1] to [0, 1]
+        ### NEW METHOD (all images, but poor quality)
+        results = self.vae_proxy[0].embed_to_img(ms_h_BChw, all_to_max_scale=True, last_one=False)
+        results = [result.add_(1).mul_(0.5) for result in results]
 
-        results = self.vae_proxy[0].fhat_to_img(f_hat).add_(1).mul_(0.5)
+        for result in results:
+            print(f"result.shape: {result.shape}")
+
+        ### OLD METHOD (only last image)
+        # results = self.vae_proxy[0].fhat_to_img(f_hat).add_(1).mul_(0.5)
 
         for b in self.blocks:
             b.attn.kv_caching(False)
