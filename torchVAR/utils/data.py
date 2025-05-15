b5f45c8f1b7a1c505b18890343797e740a7cf146
Oskar Jorgensen <oskarjorgensen@gmail.com>
2025-05-15 23:39:53 +0200

    add debug statement

--- torchVAR/utils/data.py
+++ torchVAR/utils/data.py
@@ -148,6 +148,7 @@ class NumpyDataset(torch.utils.data.Dataset):
         img = self.data[idx]
         # convert it to a PIL image
         img = img.astype(np.uint8)
+        print(img.shape)
         img = PImage.fromarray(img).convert("RGB")
         print(img.size)
         # Convert from (H, W, C) to (C, H, W) if needed
