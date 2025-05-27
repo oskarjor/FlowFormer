a16f9ac642bfac39731c2e70e22b96bcf12c0690
Oskar Jorgensen <oskarjorgensen@gmail.com>
2025-05-27 18:56:02 +0200

    add script for getting images illustrations

---
+++ scripts/illustrations/get_batch_of_images.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name="get_batch_of_images"   # Sensible name for the job
+#SBATCH --account=share-ie-idi      # Account for consumed resources
+#SBATCH --partition=CPUQ
+#SBATCH --nodes=1               # Number of nodes
+#SBATCH --ntasks-per-node=1              # Number of tasks
+#SBATCH --time=00-00:10:00    # Upper time limit for the job (DD-HH:MM:SS)
+#SBATCH --mem=100G
+#SBATCH --output=%j_illustrations.out
+#SBATCH --mail-user=oskarjor@ntnu.no
+#SBATCH --mail-type=NONE
+
+module load Python/3.10.8-GCCcore-12.2.0
+source /cluster/home/oskarjor/.virtualenv/flowformer/bin/activate
+
+python illustrations/get_batch_of_images.py \
+    --folder_path ./var_d30_imagenet/val \
+    --grid_size 8
