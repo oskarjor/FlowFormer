b3c69643ee0d3f94e746fdff7f635a57b4cf6c74
Oskar Jorgensen <oskarjorgensen@gmail.com>
2025-05-27 10:40:42 +0200

    add script for naive sampling

---
+++ scripts/naive_sample_512.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name="256->512_train_sr"   # Sensible name for the job
+#SBATCH --account=share-ie-idi      # Account for consumed resources
+#SBATCH --partition=GPUQ
+#SBATCH --gres=gpu:h100:1            # Number of GPUs
+#SBATCH --nodes=1               # Number of nodes
+#SBATCH --ntasks-per-node=1              # Number of tasks
+#SBATCH --time=00-00:10:00    # Upper time limit for the job (DD-HH:MM:SS)
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=100G
+#SBATCH --output=%j.out
+#SBATCH --mail-user=oskarjor@ntnu.no
+#SBATCH --mail-type=NONE
+
+module load Python/3.10.8-GCCcore-12.2.0
+source /cluster/home/oskarjor/.virtualenv/flowformer/bin/activate
+
+pwd
+export WANDB_API_KEY=$(cat ~/FlowFormer/.wandb_api_key)
+
+python train_SR/naive_upscale.py \
+	--save_dir="./output/naive/$SLURM_JOB_ID" \
+	--data_path="./var_d30_imagenet/" \
+	--naive_upscaling "nearest" \
+	--file_format "png"
