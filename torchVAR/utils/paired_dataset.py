74e7f1d739b14d5029601b62ffc344999ed379c5
Oskar Jorgensen <oskarjorgensen@gmail.com>
2025-10-21 13:03:32 +0200

    use final_reso for both

--- torchVAR/utils/paired_dataset.py
+++ torchVAR/utils/paired_dataset.py
@@ -201,9 +201,7 @@ def build_paired_dataset(
         # )
         synthetic_transform = transforms.Compose(
             [
-                transforms.Resize(
-                    round(image_size * 1.125), interpolation=interpolation
-                ),
+                transforms.Resize(image_size, interpolation=interpolation),
                 transforms.CenterCrop(image_size),
                 transforms.ToTensor(),
                 normalize_01_into_pm1,
