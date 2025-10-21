85f4b5603a986ba8f25dd6f7db5c32d29df5de9d
Oskar Jorgensen <oskarjorgensen@gmail.com>
2025-10-21 13:01:29 +0200

    fix typo

--- torchVAR/utils/paired_dataset.py
+++ torchVAR/utils/paired_dataset.py
@@ -213,7 +213,7 @@ def build_paired_dataset(
     if real_transform is None:
         real_transform = transforms.Compose(
             [
-                transforms.Resize(image_size, nterpolation=InterpolationMode.LANCZOS),
+                transforms.Resize(image_size, interpolation=InterpolationMode.LANCZOS),
                 transforms.CenterCrop(image_size),
                 transforms.ToTensor(),
                 normalize_01_into_pm1,
